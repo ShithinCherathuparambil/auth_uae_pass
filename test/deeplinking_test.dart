666b567c122d344da91de309ce6b096adf17a5aa
Shithin Cp <shithincp@gmail.com>
2026-04-15 22:48:19 +0530

    chore: sanitize project and bump version to 0.0.3

--- test/deeplinking_test.dart
+++ test/deeplinking_test.dart
@@ -3,8 +3,8 @@ import 'package:auth_uae_pass/src/uae_pass_mobile_api.dart';
 
 void main() {
   test('rewrite fails intentionally to print urls', () {
-    final initialDeepLink = Uri.parse('uaepassstg://digitalid.ae?successURL=https://highlight.app/callback?status=ok&failureURL=https://highlight.app/callback?status=error');
-    final spRedirect = Uri.parse('https://highlight.app/callback');
+    final initialDeepLink = Uri.parse('uaepassstg://digitalid.ae?successURL=https://uaepassdemo.com/callback?status=ok&failureURL=https://uaepassdemo.com/callback?status=error');
+    final spRedirect = Uri.parse('https://uaepassdemo.com/callback');
     
     final rewritten = rewriteUaePassDeepLinkForSp(
       uaePassDeepLink: initialDeepLink,
@@ -13,6 +13,6 @@ void main() {
     );
     
     final successurl = rewritten.queryParameters['successurl'];
-    expect(successurl, startsWith('https://highlight.app/resume_authn?url=https%3A%2F%2Fhighlight.app'));
+    expect(successurl, startsWith('https://uaepassdemo.com/resume_authn?url=https%3A%2F%2Fuaepassdemo.com'));
   });
 }
